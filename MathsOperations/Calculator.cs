40e6fd38a205597e194ef713c37a86d66fca7579
ravindrabn <63568266+ravindrabn@users.noreply.github.com>
2020-06-19 21:44:48 +1000

    master

---
+++ MathsOperations/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MathsOperations
+{
+    public static class Calculator
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply (int a, int b)
+        {
+            return a * b;
+            
+
+        }
+        public static int ivision(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ MathsOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathsOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
